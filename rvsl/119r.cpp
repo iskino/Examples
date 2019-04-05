55968df4f01348f19bb9d70bff024c1dd0a96892
iskino <isko.85@mail.ru>
2019-04-05 21:05:35 +0400

    Делегирующие конструкторы

---
+++ rvsl/119r.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <iostream>
+
+class Employee
+{
+private:
+    int m_id;
+    std::string m_name;
+
+public:
+    Employee(int id=0, const std::string &name=""):
+        m_id(id), m_name(name)
+    {
+        std::cout << "Employee " << m_name << " created.\n";
+    }
+
+    // Используем делегирующие конструкторы для минимизации дублированного кода
+    Employee(const std::string &name) : Employee(0, name) { }
+};
+
+int main()
+{
+    Employee a;
+    Employee b("Ivan");
+
+    return 0;
+}
